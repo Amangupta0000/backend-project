b433030c40c9bf66160e5ba620bcb9e33ee2ed64
Amangupta0000 <amangupta1a2b3c@gmail.com>
2024-07-09 02:02:42 +0530

    user register controller completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,61 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async ( req,  res) => {
-    res.status(200).json({
-        message : "ok"
+const registerUser = asyncHandler(async (req, res) => {
+    // get user detials from fronted
+    const { fullName, email, username , password } = req.body
+    console.log("email : ", email)
+
+    // validation - not empty 
+    if ( [fullName ,email , username , password].some((fields)=> fields?.trim() === "") ) {
+         return new ApiError(400 , "All fields are required")
+    }
+    // check if user already exitst or not : username & email unique
+    const userExist = await User.findOne({
+            $or : [ {email} , { username}]
     })
+    if (userExist) {
+        return new ApiError(409 , "User with email or username already exist")
+    }
+    // check for images and avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLacalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        return new ApiError(400 , "Avatar image is required")
+    }
+    //upload them to cloudinary , avatar
+    const avatar =await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLacalPath)
+    if (!avatar) {
+        return new ApiError(400 , "Avatar image is required")
+    }
+    // create user object - create entry in db 
+    const user = await User.create({
+        fullName,
+        email,
+        username,
+        password,
+        avatar: avatar.url,
+        coverImage : coverImage?.url || ""
+
+    })
+    // remove pswd and refresh token from response
+    //check user create succesfully
+    const createdUser =await User.findById(user._id).select("-password -refreshToken")
+    
+    if (!createdUser) {
+        return new ApiError(500 , "Something went wrong while registering the user")
+    }
+    // return response
+
+    return res.status(201).json(
+        new ApiResponse(200 , createdUser , "User registered Successfully")
+    )
+    
 })
 
 export {registerUser }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,42 +1,43 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
 
- // Configuration
- cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+// Configuration
+cloudinary.config({
+    cloud_name: 'dec8t0sqo',
+    api_key: '662518676711917',
+    api_secret: '0NRDV93czAWuwiqg-o7liSw_HZ0'
 });
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
         //upload file on cloudinary
         const uploadResult = await cloudinary.uploader
-       .upload(
-          localFilePath, {
-              resource_type : 'auto',
-           }
-        )
+            .upload(
+                localFilePath, {
+                resource_type: 'auto',
+            },
+
+            )
         //file has been uploaded successfully to cloudinary
         console.log("file has been uploaded successfully to cloudinary", uploadResult.url);
         return uploadResult
-      }
-    catch(error)  {
+    }
+    catch (error) {
         console.log(error);
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file from server as error occurred
+        return null
+    };
 
-       };
-    
-    
-    
+
+
     // // Optimize delivery by resizing and applying auto-format and auto-quality
     // const optimizeUrl = cloudinary.url('shoes', {
     //     fetch_format: 'auto',
     //     quality: 'auto'
     // });
-    
+
     // console.log(optimizeUrl);
-    
+
     // // Transform the image: auto-crop to square aspect_ratio
     // const autoCropUrl = cloudinary.url('shoes', {
     //     crop: 'auto',
@@ -44,7 +45,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     //     width: 500,
     //     height: 500,
     // });
-    
+
     // console.log(autoCropUrl);    
 }
 
